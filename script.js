696a80ff0add46966da4f6a223c82634481e6826
Mordecai119513 <mordecai0304@gmail.com>
2024-03-15 17:16:01 +0800

    Crescendo Lab API Test

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ document.addEventListener("DOMContentLoaded", function () {
             .getProfile()
             .then(displayUserProfile)
             .catch((err) => {
-              console.error("获取用户信息失败", err);
+              console.error("獲取用戶訊息失敗", err);
             });
         }
       })
@@ -39,3 +39,43 @@ document.addEventListener("DOMContentLoaded", function () {
   // 呼叫initializeLiff函數並傳入您的LIFF ID
   initializeLiff("2004081412-31AxDOb0");
 });
+
+// 漸強實驗室串接測試
+
+function sendToCrescendo(dataToSend) {
+  const crescendoAPIKey =
+    "$ dredd init -r apiary -j apiaryApiKey:e7be0eeff9489ea8ebe039db88a57561 -j apiaryApiName:lifftest"; // 漸強API金鑰
+  const crescendoAPIUrl = "https://liff.line.me/2004081412-31AxDOb0"; // LIFF的URL
+
+  fetch(crescendoAPIUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${crescendoAPIKey}`,
+    },
+    body: JSON.stringify(dataToSend),
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error("Network response was not ok.");
+    })
+    .then((data) => {
+      console.log("漸強API已成功接收訊息", data);
+    })
+    .catch((error) => {
+      console.error("漸強API調用失敗:", error);
+    });
+}
+
+const messageData = {
+  userId: profile.userId, // 使用從LIFF獲取的實際LINE用戶ID
+  messages: [
+    {
+      type: "text",
+      text: "這是從LIFF應用程式發送的測試訊息",
+    },
+  ],
+};
+sendToCrescendo(messageData); // 發送訊息給漸強
